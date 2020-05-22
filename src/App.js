ef75de189ad4a4c483f9aa683b0263c46ad7e83a
hao <324677869@qq.com>
2020-05-22 12:52:45 +0800

    generate react-app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import game from "./game/game.js";
 
 function App() {
+  console.log(game.introduce);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          edit
+          <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
---
+++ src/game/game.js
@@ -0,0 +1,12 @@
+var game = {
+  introduce: "Factoridle is a fan game of factorio",
+  status: 0
+  /*
+  0:就绪
+  1:开始
+  2:载入中
+  3:暂停
+  */
+};
+
+export default game;
