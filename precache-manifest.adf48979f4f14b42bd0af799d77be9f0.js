01de8eb2b54f6ee8ccddbc2e82a80f09ec318e8a
hao <324677869@qq.com>
2020-05-23 16:56:46 +0800

    Updates

---
+++ precache-manifest.adf48979f4f14b42bd0af799d77be9f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c792d6ced1f1d7c15d654ccff4bc7ab",
+    "url": "/factoridle/index.html"
+  },
+  {
+    "revision": "078716e59c05d191f65d",
+    "url": "/factoridle/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c8a3fd9d29360ed700bd",
+    "url": "/factoridle/static/js/2.f26fb2a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/factoridle/static/js/2.f26fb2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "078716e59c05d191f65d",
+    "url": "/factoridle/static/js/main.2def0a4f.chunk.js"
+  },
+  {
+    "revision": "0284b592b349f350d9d9",
+    "url": "/factoridle/static/js/runtime-main.dfbaba25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/factoridle/static/media/logo.5d5d9eef.svg"
+  }
+]);
